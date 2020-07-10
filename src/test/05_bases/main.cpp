eaa1b053796322fd20e8ccc33427a18c25a2f4b6
Ubpa <641614112@qq.com>
2020-07-10 20:34:44 +0800

    subclasses -> bases

--- src/test/05_bases/main.cpp
+++ src/test/05_bases/main.cpp
@@ -24,11 +24,11 @@ template<>
 struct Type<A> {
 	static constexpr std::string_view name = "A";
 	using type = A;
-	static constexpr TypeList subclasses = {};
+	static constexpr TypeList bases = {};
 
 	static constexpr FieldList fields = FieldList{
 		Field{"a", &A::a, AttrList{} }
-	}.UnionTypeList(subclasses);
+	}.UnionTypeList(bases);
 
 	static constexpr AttrList attrs = {};
 };
@@ -37,11 +37,11 @@ template<>
 struct Type<B> {
 	static constexpr std::string_view name = "B";
 	using type = B;
-	static constexpr TypeList subclasses = {};
+	static constexpr TypeList bases = {};
 
 	static constexpr FieldList fields = FieldList{
 		Field{"b", &B::b, AttrList{} }
-	}.UnionTypeList(subclasses);
+	}.UnionTypeList(bases);
 
 	static constexpr AttrList attrs = {};
 };
@@ -50,11 +50,11 @@ template<>
 struct Type<C> {
 	static constexpr std::string_view name = "C";
 	using type = C;
-	static constexpr TypeList subclasses = { Type<A>{} };
+	static constexpr TypeList bases = { Type<A>{} };
 
 	static constexpr FieldList fields = FieldList{
 		Field{"c", &C::c, AttrList{} }
-	}.UnionTypeList(subclasses);
+	}.UnionTypeList(bases);
 
 	static constexpr AttrList attrs = {};
 };
@@ -63,11 +63,11 @@ template<>
 struct Type<D> {
 	static constexpr std::string_view name = "D";
 	using type = D;
-	static constexpr TypeList subclasses = { Type<B>{}, Type<C>{} };
+	static constexpr TypeList bases = { Type<B>{}, Type<C>{} };
 
 	static constexpr FieldList fields = FieldList{
 		Field{"d", &D::d, AttrList{} }
-	}.UnionTypeList(subclasses);
+	}.UnionTypeList(bases);
 
 	static constexpr AttrList attrs = {};
 };
@@ -77,7 +77,7 @@ void dump(size_t depth = 0) {
 	for (size_t i = 0; i < depth; i++)
 		cout << "  ";
 	cout << Type<T>::name << endl;
-	Type<T>::subclasses.ForEach([depth](auto t) {
+	Type<T>::bases.ForEach([depth](auto t) {
 		dump<typename decltype(t)::type>(depth + 1);
 	});
 }
